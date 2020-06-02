a9a077fa9034876279d8d48653ad9dc919b960d0
mahmudamen <mahmudamen@gmail.com>
2020-06-02 08:06:04 +0200

    first-commit

---
+++ InternetConnection/src/main/java/com/meno/internetconnection/InternetConnection.java
@@ -0,0 +1,31 @@
+package com.meno.internetconnection;
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.text.TextPaint;
+import android.text.style.TypefaceSpan;
+
+public class InternetConnection {
+
+    /**
+     * CHECK WHETHER INTERNET CONNECTION IS AVAILABLE OR NOT
+     */
+    public static boolean checkConnection(Context context) {
+        final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connMgr != null) {
+            NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
+
+            if (activeNetworkInfo != null) { // connected to the internet
+                // connected to the mobile provider's data plan
+                if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                    // connected to wifi
+                    return true;
+                } else return activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+            }
+        }
+        return false;
+    }
+}
